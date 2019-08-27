cdbc40c35764be70a33750d6ba8ba3faf57baadb
Ryan Murray <ryanmurrayemail@gmail.com>
2019-08-28 07:07:49 +1000

    updated work page

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -8,7 +8,7 @@
             A collection of projects I've worked on
         </h2>
         <hr class="my-5">
-
+        <h3>I'm just updating this page, it will be back very soon.</h3>
     </div>
 
 @stop
