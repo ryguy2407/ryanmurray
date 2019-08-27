21fbe26224fa128e5680233a4ecc102a9d1d6e73
Ryan Murray <ryanmurrayemail@gmail.com>
2019-08-27 18:21:17 +1000

    added header fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -32,7 +32,7 @@
                 <a class="nav-link" href="/about">About Me</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Articles</a>
+                <a class="nav-link" href="/blog">Blog</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">My Work</a>
