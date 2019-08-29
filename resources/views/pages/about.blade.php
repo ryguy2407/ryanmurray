e382d757d3e62ac7053ea7ef7e82feff94e6c9e6
Ryan Murray <ryanmurrayemail@gmail.com>
2019-08-29 12:34:55 +1000

    updated about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -10,6 +10,21 @@
         <hr class="my-5">
         <div class="row">
             <div class="col">
+                <img src="/img/elliot-theo.jpg" style="width: 100%;" class="mb-5 rounded mx-auto d-block">
+            </div>
+            <div class="col">
+                <img src="/img/wedding.jpg" style="width: 100%;" class="mb-5 rounded mx-auto d-block">
+            </div>
+            <div class="col">
+                <img src="/img/milly.jpg" style="width: 100%;" class="mb-5 rounded mx-auto d-block">
+            </div>
+            <div class="col">
+                <img src="/img/family.jpg" style="width: 100%;" class="mb-5 rounded mx-auto d-block">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
                 <p>
                     Since the arrival of our first child in 2015, I have taken time
                     away from the office to raise our two children. In that time
